32b4eb5aa352cb420f2a19a1571332736f2f4d34
tomast1337 <vycasnicolas@gmail.com>
2019-11-28 22:00:45 -0300

    Adicionado Classe Produto TODO 🏗

--- src/Negocio/Produto.java
+++ src/Negocio/Produto.java
@@ -10,5 +10,49 @@ package Negocio;
  * @author tomast1337
  */
 public class Produto {
-    
+    private int codigo;
+    private String nome;
+    private String tipo;
+    private String descricao;
+    private int preco;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
 }
