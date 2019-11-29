9863dbc001ae16958c1155744bb37fa1d28e4aa7
tomast1337 <vycasnicolas@gmail.com>
2019-11-28 21:58:56 -0300

    Adicionado Classe Cliente TODO 🙍‍♀️🙍‍♂️

--- src/Negocio/Cliente.java
+++ src/Negocio/Cliente.java
@@ -7,8 +7,34 @@ package Negocio;
 
 /**
  *
- * @author tomast1337
+ * @author Nicolas Vycas Nery
  */
 public class Cliente {
-    
+    private int codigo;
+    private String nome;
+    private Venda venda;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Venda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Venda venda) {
+        this.venda = venda;
+    }
 }
