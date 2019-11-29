6b0e1ff3c4cb72567c887537bd5f0da34f8f7260
tomast1337 <vycasnicolas@gmail.com>
2019-11-28 22:00:22 -0300

    Adicionado classe Itens venda TODO 📝

---
+++ src/Negocio/ItensVenda.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+/**
+ *
+ * @author Nicolas Vycas Nery
+ */
+public class ItensVenda {
+    private Venda venda;
+    private int quantidade;
+    private Produto produto;
+
+    public Venda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Venda venda) {
+        this.venda = venda;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+}
