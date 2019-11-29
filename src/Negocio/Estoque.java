ca5a333d158dae1e6351716bc605629298cc2db3
tomast1337 <vycasnicolas@gmail.com>
2019-11-28 21:59:20 -0300

    Adicionado classe estoque TODO 📦

---
+++ src/Negocio/Estoque.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+/**
+ *
+ * @author Nicolas Vycas Nery
+ */
+public class Estoque {
+    private int quantidade;
+    private Produto produto;
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+}
